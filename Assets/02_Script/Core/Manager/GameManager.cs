6b16bf5fafdd7cd7b7074f343ea7ba8c98684b31
tkfkadlsi <yeseonggim35@gmail.com>
2025-01-16 01:18:05 +0900

    2025.01.15 Day3
    오늘은 싱글톤 클래스(매니저들)과 UI 클래스들의 부모가 되는 BaseUI, 씬마다 하나 씩 있는 가장 부모의 UI에 붙는 RootUI 클래스를 만들었다.

--- Assets/02_Script/Core/Manager/GameManager.cs
+++ Assets/02_Script/Core/Manager/GameManager.cs
@@ -2,5 +2,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public RootUI RootUI { get; private set; }
+
+    public void SetRootUI(RootUI rootUI)
+    {
+        this.RootUI = rootUI;
+    }
+
 
 }
---
+++ Assets/02_Script/UI/BaseUI.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class BaseUI : BaseInit
+{
+    private Dictionary<Type, Object[]> _objects = new Dictionary<Type, Object[]>();
+
+    private void OnEnable()
+    {
+        ActiveOn();
+    }
+
+    private void OnDisable()
+    {
+        ActiveOff();
+    }
+
+    protected virtual void ActiveOn()
+    {
+        Debug.Log($"{name} active is on");
+    }
+
+    protected virtual void ActiveOff()
+    {
+        Debug.Log($"{name} active is off");
+    }
+
+    protected void Bind<T>(Type type) where T : Object
+    {
+        string[] names = Enum.GetNames(type);
+        Object[] objects = new Object[names.Length];
+
+        _objects.Add(typeof(T), objects);
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            if (typeof(T) == typeof(GameObject))
+            {
+                objects[i] = Utility.FindChild(gameObject, names[i], true);
+            }
+            else
+            {
+                objects[i] = Utility.FindChild<T>(gameObject, names[i], true);
+            }
+
+            if (objects[i] == null)
+            {
+                Debug.LogError($"{names[i]} (이)라는 오브젝트는 없습니다.");
+            }
+        }
+    }
+
+    protected T Get<T>(int idx) where T : Object
+    {
+        if (_objects.TryGetValue(typeof(T), out Object[] objects))
+        {
+            return objects[idx] as T;
+        }
+
+
+        Debug.LogError($"{typeof(T)} 타입이 Bind되지 않았습니다.");
+        return null;
+    }
+}
